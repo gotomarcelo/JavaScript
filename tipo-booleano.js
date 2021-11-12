a113626ede7cc29ee7b3d18d359b68eaab6ab7ca
Marcelo Goto <gotomarcelo98@gmail.com>
2021-11-11 21:04:52 -0300

    inserindo informações e adicionando null e undefined

--- tipo-booleano.js
+++ tipo-booleano.js
@@ -6,3 +6,9 @@ const texto1 = "alura";
 const texto2 = 'alura';
 
 console.log(texto1 === texto2);
+
+// JavaScript é case-sensitive
+//convenções, para padronizar estes aspectos do código
+//camelCase,snake_case,kebab-case,PascalCase
+//Importante: Nunca utilize espaço nem caracteres especiais, nem inicie os nomes das variáveis com números.
+//seguir as convenções é parte de desenvolver um código legível e bem escrito
---
+++ tipo-null-e-undefined.js
@@ -0,0 +1,17 @@
+//O null é um tipo especial, pois pode ser traduzido como “ausência de valor”
+let input = null
+
+if (input === null) {
+    console.log('não há informação');
+   } else {
+    console.log(input);
+   }
+
+//undefined representa “ausência de valor”
+let input2;
+console.log(input2);
+//enquanto null é um valor atribuído a uma variável que existe e foi iniciada, 
+//undefined se refere ao valor de uma variável que não foi inicializada (ou seja,
+//não foi atribuído nenhum valor a ela).
+
+//normalmente undefined é atribuida a um erro no codigo por exemplo
